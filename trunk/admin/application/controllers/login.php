5d8db8d20cb871a0a23a10080d6bd7b54f27d641
codeparking@gmail.com <codeparking@gmail.com@0e10977d-14f1-749b-5044-18f829f14c07>
2015-01-30 13:45:42 +0000

    adding the registrartion modules and login validations with js , controllers and models

--- trunk/admin/application/controllers/login.php
+++ trunk/admin/application/controllers/login.php
@@ -23,6 +23,14 @@ class Login extends CI_Controller {
 		$data['base_url'] = $this->config->item('base_url');
 		$this->load->view('login/login', $data);
 	}
+	public function checkaccess()
+	{	
+		$this->load->model('Users_model');
+		$data = array();
+		$data = $this->Users_model->checkLogin(base64_decode($this->uri->segment(3)),base64_decode($this->uri->segment(4)));
+		print "came here";
+		exit;
+	}
 }
 
 
